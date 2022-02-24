94fc4cfadf1dbf499fd7201bcf85f51352aca5c9
AishwaryaBirla <53036180+AishwaryaBirla@users.noreply.github.com>
2022-02-24 18:41:12 +0530

    filter

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ function mapFunction(inputArray){
     for (let i in inputArray){
         outputArray.push(inputArray[i]*inputArray[i]);}
         
-    return outputArray
+    return outputArray;
 }
 function foreachFunction(inputArray){
     for (let i in inputArray){
@@ -14,8 +14,16 @@ function foreachFunction(inputArray){
             }
         return sum;
 }
+function filterFunction(inputArray, min){
+    for (let i in inputArray){
+        if (inputArray[i]>min){
+            outputArray.push(inputArray[i]);
+        }
+            }
+            return outputArray;
+}
 
 
 // console.log(mapFunction(inputArray));
-console.log(foreachFunction(inputArray));
+console.log(filterFunction(inputArray,30));
 
